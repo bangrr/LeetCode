95bb3ee1d2ca1f5db7cb2bff1e5fbf053d19ef94
Bangrr <96755285+Bangrr@users.noreply.github.com>
2023-02-03 23:18:30 +0900

    Time: 3 ms (83.97%), Space: 42.4 MB (77.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         int[] sArr = new int[26];
-        int[] tArr = new int[26];
         for (int i=0; i<s.length(); i++) {
             sArr[s.charAt(i)-97]++;
         }
         for (int i=0; i<t.length(); i++) {
-            tArr[t.charAt(i)-97]++;
+            sArr[t.charAt(i)-97]--;
         }
         for (int i=0; i<26; i++) {
-            if (sArr[i] != tArr[i]) {
+            if (sArr[i] != 0) {
                 return false;
             }
         }
